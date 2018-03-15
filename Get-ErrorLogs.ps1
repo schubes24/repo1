08075852b10a63540d273c6100cd7d0fde82d665
Josh Schubert <josh.schubert@hotmail.com>
2018-03-14 19:27:37 -0500

    Delete Get-ErrorLogs.ps1

--- Get-ErrorLogs.ps1
+++
@@ -1,8 +0,0 @@
-$smtp_server = “exch2.totaltool.int”;
- $to = “matt.bagan@totaltool.com”;
- $from = “administrator@totaltool.com”;
- $subject = “System Error Log Report”;
- $date = get-date
- $body = "Error logs from $date";
-  Get-EventLog System -EntryType Error -computername Exch2,ttdc03,ttfs02,ttsql,veeam7,ttspice,ttwdm, ttcrib,ttcsg2,ttfieldpoint,ttimaging,ttmdm,ttsharepoint,ttweb03 -After (Get-Date).AddDays(-1) |Select -property machinename,Timegenerated,eventID,Message,source | convertto-html |out-file c:\scripts\ErrorLogs.htm
-  send-mailmessage -to $to -from $from -subject $subject -body $body -attachments "c:\scripts\ErrorLogs.htm" -smtpserver $smtp_server
