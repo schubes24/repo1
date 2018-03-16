0f3b7738ed5d6683c2af0bf8ac0ba295d29d1170
Josh Schubert <josh.schubert@hotmail.com>
2018-03-16 06:52:57 -0500

    Delete Get-EarthPornImages.ps1

--- Get-EarthPornImages.ps1
+++
@@ -1,5 +0,0 @@
-﻿Import-Module BitsTransfer
-$webiste = "www.reddit.com/r/earthporn"
-$destination = "$ENV:USERPROFILE\Documents\Earthporn"
-IF(!(Test-Path $destination)) {New-Item -Path $destination -ItemType Directory -Force | Out-Null}
-((Invoke-WebRequest -Uri $webiste).links | Where { $_.href -match ".jpg" } | Where { $_.class -match "title" }).href | Foreach {Start-BitsTransfer -Source $_ -Destination $destination }
